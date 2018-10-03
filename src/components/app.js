b71544d0b63f94f332607ad28d2dbdbad1d75a8a
Tanner Noble <tannernoble@gmail.com>
2018-10-03 16:28:55 -0500

    rendering JSX based on app state

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,26 @@ import Clock from './clock';
 
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: false
+    }
+  }
+
+  renderItems = function() {
+    if(this.state.active) {
+      return [
+        <Clock/>,
+        
+      ] 
+    } else {
+        return Button('Generate Countdown', () => this.setState({ active: true }))
+    }
+  }.bind(this)
+
   render() {
 
     // return <div className=""><Clock/></div>
@@ -22,8 +42,8 @@ export default class App extends Component {
         <div className="grid__skew-light-three-box"></div>
 
         <Picker />
-        { Button('Generate Countdown') }
-        <Clock />
+        { this.renderItems() }
+        
       </div>
     );
   }

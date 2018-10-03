36dc03a81bf422aa9d18818dcfbb35610541e922
Tanner Noble <tannernoble@gmail.com>
2018-10-03 00:07:35 -0500

    app setup

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Birthday Countdown</h1>
+        <h2>by Tnob</h2>
+      </div>
+    );
+  }
+}

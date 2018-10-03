6f9655064c4ccdff68dfbba4db74548128e054b9
Tanner Noble <tannernoble@gmail.com>
2018-10-03 15:23:59 -0500

    built and styled clock component

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Picker from './picker';
 import Button from './button';
+import Clock from './clock';
 
 
 export default class App extends Component {
@@ -19,6 +20,7 @@ export default class App extends Component {
 
         <Picker />
         { Button('Generate Countdown') }
+        <Clock />
       </div>
     );
   }

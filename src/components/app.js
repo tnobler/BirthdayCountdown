920926e0595cd5dbbb6956bb5c94178bf6feb35d
Tanner Noble <tannernoble@gmail.com>
2018-10-04 09:35:04 -0500

    fixed all cases for birthday and currentday calculation

--- src/components/app.js
+++ src/components/app.js
@@ -37,27 +37,41 @@ export default class App extends Component {
   }.bind(this)
 
   handleGenerate = function () {
-    this.setState({ active: true })
-    // Set the date we're countin down
+    this.setState({ active: true });
 
-    var countDownDate = this.state.startDate.toDate().getTime();
+    var bday = this.state.startDate.toDate();
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var birthMonth = bday.getMonth();
 
-    // Update the count down every 1 second
+    if(birthMonth > currentMonth) {
+      bday.setFullYear(today.getFullYear())
+    } else if(birthMonth < currentMonth) {
+      bday.setFullYear(today.getFullYear() + 1)
+    } else if(birthMonth == currentMonth) {
+      var currentDay = today.getDate();
+      var birthDay = bday.getDate();
+
+      if(birthDay > currentDay) {
+        bday.setFullYear(today.getFullYear())
+      }
+      if(birthDay <= currentDay) {
+        bday.setFullYear(today.getFullYear() + 1)
+      } 
+    }
+
+    var countDownDate = bday.getTime();
+
     this.timer = setInterval(function () {
 
-      // Get todays date and time
-      var now = new Date().getTime();
-
-      // Find the distance between now and the count down date
+      var now = today.getTime();
       var distance = countDownDate - now;
 
-      // Time calculations for days, hours, minutes and seconds
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      // Output the result in an element with id="demo"
       const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
       const timeRemaining = {
         days,
@@ -67,7 +81,6 @@ export default class App extends Component {
       }
       this.setState({ timeRemaining })
 
-      // If the count down is over, write some text
       if (distance < 0) {
         clearInterval(this.timer);
         // document.getElementById("demo").innerHTML = "EXPIRED";
